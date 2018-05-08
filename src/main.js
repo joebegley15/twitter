a82162e2ce21c4f05b80212cdc99e7895484c5b8
Joe Begley <joebegley15@gmail.com>
2018-05-08 16:47:59 -0400

    Cleaned up the code on a couple functions

--- src/main.js
+++ src/main.js
@@ -49,46 +49,6 @@ function tradingHours(data,ticker) {
 
 }
 
-function dmSlide(data,ticker,userName) {
-	var dataObj = JSON.parse(data);
-	var quote = dataObj['quote'];
-	var news = dataObj['news'];
-	var tweet = '';
-	if (quote) {
-		var name = quote['companyName'];
-		var week52High = quote['week52High']; 
-		var week52Low = quote['week52Low']; 
-		var price = quote['latestPrice']; 
-		var change = Math.round(quote['change'] * 100) / 100; 
-		var symbol = quote['symbol']; 
-		var previousClose = quote['previousClose'];
-		var pctDay = Math.round(quote['changePercent'] * 10000) / 100;
-		var fromLow = Math.floor(((price - week52Low) / week52Low) * 100);
-		var fromHigh = Math.floor(((week52High - price) / week52High) * 100);
-		var upDwn = 'up ';
-		var sentenceEnders = ['today', 'on the day', 'so far today', 'from opening'];
-		if (change < 0) {
-			var upDwn = 'down ';
-			change = change * -1;
-		}
-		tweet = name + ' is ' + upDwn + '$' + change + ' (' + pctDay + '%) ' + sentenceEnders[Math.floor(Math.random() * sentenceEnders.length)] + '.';
-		if (fromLow > 25 && fromHigh < fromLow) {
-			tweet += ' The stock is up more than ' + fromLow + '% from it\'s 52 week low.'	
-		} else if (fromHigh > 25) {
-			tweet += ' The stock is down more than ' + fromHigh + '% from it\'s 52 week high.'	
-		}
-	}
-	// var hashTags = ['stocks', 'trading','iexAPI']; ' #' + hashTags[Math.floor(Math.random() * hashTags.length)] + 
-
-	tweet += ' #' + ticker;
-	if (tweet) {
-		if (news) {
-			tweet += ' ' + news[0]['url'];
-		}
-		slide(tweet,'jdipi');
-	}
-}
-
 function stockQuery(ticker,reqType){
 	https.get('https://api.iextrading.com/1.0/stock/' + ticker + '/batch?types=quote,news,chart&range=1m&last=10', (resp) => {
 	  let data = '';
--- src/twitter.js
+++ src/twitter.js
@@ -44,35 +44,47 @@ function getFollowers(screenName) {
 	})
 }
 
-// const autoDM = () => {
-//   const stream = Twit.stream("user");
-//   console.log("Start Sending Auto Direct Message 🚀🚀🚀");
-//   stream.on("follow", SendMessage);
-// };
+function dmFinancial(data,ticker,userName) {
+	var dataObj = JSON.parse(data);
+	var quote = dataObj['quote'];
+	var news = dataObj['news'];
+	var tweet = '';
+	if (quote) {
+		var name = quote['companyName'];
+		var week52High = quote['week52High']; 
+		var week52Low = quote['week52Low']; 
+		var price = quote['latestPrice']; 
+		var change = Math.round(quote['change'] * 100) / 100; 
+		var symbol = quote['symbol']; 
+		var previousClose = quote['previousClose'];
+		var pctDay = Math.round(quote['changePercent'] * 10000) / 100;
+		var fromLow = Math.floor(((price - week52Low) / week52Low) * 100);
+		var fromHigh = Math.floor(((week52High - price) / week52High) * 100);
+		var upDwn = 'up ';
+		var sentenceEnders = ['today', 'on the day', 'so far today', 'from opening'];
+		if (change < 0) {
+			var upDwn = 'down ';
+			change = change * -1;
+		}
+		tweet = name + ' is ' + upDwn + '$' + change + ' (' + pctDay + '%) ' + sentenceEnders[Math.floor(Math.random() * sentenceEnders.length)] + '.';
+		if (fromLow > 25 && fromHigh < fromLow) {
+			tweet += ' The stock is up more than ' + fromLow + '% from it\'s 52 week low.'	
+		} else if (fromHigh > 25) {
+			tweet += ' The stock is down more than ' + fromHigh + '% from it\'s 52 week high.'	
+		}
+	}
+	// var hashTags = ['stocks', 'trading','iexAPI']; ' #' + hashTags[Math.floor(Math.random() * hashTags.length)] + 
 
-// const SendMessage = user => {
-//   const { screen_name, name } = user.source;
-
-//   const obj = {
-//     screen_name,
-//     text: GenerateMessage(name)
-//   };
-//   // the follow stream track if I follow author person too.
-//   if (screen_name != my_user_name) {
-//     console.log(" 🎉🎉🎉🎉 New Follower  🎉🎉🎉🎉🎉 ");
-//     setTimeout(() => {
-//       Twit.post("direct_messages/new", obj)
-//         .catch(err => {
-//           console.error("error", err.stack);
-//         })
-//         .then(result => {
-//           console.log(`Message sent successfully To  ${screen_name}  💪💪`);
-//         });
-//     }, timeout);
-//   }
-// };
+	tweet += ' #' + ticker;
+	if (tweet) {
+		if (news) {
+			tweet += ' ' + news[0]['url'];
+		}
+		slide(tweet,'jdipi');
+	}
+}
 
 module.exports.postTweet = postTweet;
 module.exports.slide = slide;
 module.exports.getFollowers = getFollowers;
-// module.exports.autoDM = autoDM;
+module.exports.dm
